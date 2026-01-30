18f7868441437186c10a3df871d35bab9cea0439
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2026-01-30 19:12:38 +0100

    odd or even number

---
+++ 001_Basics/006_Error-Handling/exercises/04-odd-or-even/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Odd or Even
+//
+//  1. Get a number from the command-line.
+//
+//  2. Find whether the number is odd, even and divisible by 8.
+//
+// RESTRICTION
+//  Handle the error. If the number is not a valid number,
+//  or it's not provided, let the user know.
+//
+// EXPECTED OUTPUT
+//  go run main.go 16
+//    16 is an even number and it's divisible by 8
+//
+//  go run main.go 4
+//    4 is an even number
+//
+//  go run main.go 3
+//    3 is an odd number
+//
+//  go run main.go
+//    Pick a number
+//
+//  go run main.go ABC
+//    "ABC" is not a number
+// ---------------------------------------------------------
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Pick a number")
+		return
+	}
+
+	number := os.Args[1]
+	n, err := strconv.Atoi(number)
+	
+	if err != nil {
+		fmt.Printf("%q is not a number\n", number)
+		return
+	}
+
+	if n%2 == 0 {
+		fmt.Printf("%d is an even number", n)
+		if n%8 == 0 {
+			fmt.Printf(" and it's divisible by 8")
+		}
+		fmt.Println("")
+	} else {
+		fmt.Printf("%d is an odd number\n", n)
+	}
+}
